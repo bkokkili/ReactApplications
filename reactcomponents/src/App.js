7d0bffddd4e02da6d78bd587e9903799be779031
bkokkili <venkatbalaji504@gmail>
2023-11-26 22:47:20 +0530

    ReactChanges

---
+++ reactcomponents/src/App.js
@@ -0,0 +1,22 @@
+import './App.css'
+function Header(){
+  return(
+    <div id="headerDiv">
+      <h1>NETFLIX</h1>
+    </div>
+  )
+}
+
+function Movies(){
+  return(
+    <div id="movies">
+            <img id ="imgWidth" src="https://occ-0-1492-3663.1.nflxso.net/dnm/api/v6/tx1O544a9T7n8Z_G12qaboulQQE/AAAABVzjTL18C-_NmR9Jhm0mj2r-PS1cVKLUi1JK3E5edObxhxMMvtqOJANtQIBXIVsa8Wj1pAota7LmAQ6nriJqXYvOAQfFBMfwDzWIFPNmwtc.webp?r=cd6" alt=""/>
+            
+            <img id ="imgWidth" src="https://occ-0-1492-3663.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABYkeDRiLOTV9ng02Yt318HsGYmKvfjxOCXgqb12CV0VoUdiUx1weAXlGQ4hBtt24dNFfYC60o9IvMkA9y3rRJmAK6Q7o8xaQDh8.webp?r=6f8" alt=""/>
+            <img id ="imgWidth" src="https://occ-0-1492-3663.1.nflxso.net/dnm/api/v6/tx1O544a9T7n8Z_G12qaboulQQE/AAAABe1Ajk0NDhd4FTU75vFCz55qpgiaxfJcD6iJzfg4e9szATTJIJNLkqoyaR-VsRx5cbQne16VdRYOVVZhC678Cgfm_lKbndGkgvJIta4F9jM.webp?r=6d7" alt=""/>
+            <img id ="imgWidth" src="https://occ-0-1492-3663.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABU_HgwW2TPrtrw2Bupf53eQSwJtLKMifz9zmpErry9jhFuGZlBSNz6Acc0b-DlGT2jg3A25pamsEI36k8wGIpTvGuAu2Wv6lktwZNpEtywjB_dxG1-dO03eSdoR0YcwlaeuxA_qA3V1wAFf28wZt_NOY3_yKgJCO1LY.webp?r=8da" alt=""/>
+    </div>
+  )
+}
+export {Movies} 
+export default Header;
