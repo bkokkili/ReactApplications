ee634a27ad23c65ac0227c58b4787e909f5928d1
bkokkili <venkatbalaji504@gmail>
2023-12-09 16:46:30 +0530

    react changes

---
+++ moviesearch/src/Components/Movies.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import MoviesList from "./MoviesList";
+import TextField from '@mui/material/TextField';
+
+const Movies = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [moviesData, setMoviesData] = useState([]);
+
+  useEffect(() => {
+    let API_key = "7722ae9b";
+    fetch(`https://www.omdbapi.com/?s=${searchTerm}&apikey=${API_key}`)
+      .then((response) => response.json())
+      .then((data) => setMoviesData(data.Search));
+  }, [searchTerm]);
+
+  return (
+    <div>
+      <h1 style={{ textAlign: "center", color: "blue" }}>Movies Search</h1>
+      <header style={{ textAlign: "center" }}>
+
+        <TextField id="outlined-basic" label="Enter Movie" variant="outlined"  onChange={(e) => setSearchTerm(e.target.value)}/>
+       
+      </header>
+      <MoviesList data={moviesData} />
+    </div>
+  );
+};
+
+export default Movies;
